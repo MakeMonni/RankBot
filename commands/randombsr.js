446ebd680a444553063e0d67541a2cec5009215b
L5392 <L5392@student.jamk.fi>
2022-02-26 18:04:51 +0200

    Add njs to randombsr

--- commands/randombsr.js
+++ commands/randombsr.js
@@ -3,13 +3,22 @@ const Discord = require("discord.js");
 
 class Randombsr extends Command {
     async run(client, message, args) {
-        const map = await client.db.collection("beatSaverLocal").aggregate([{ $match: { automapper: false } },{ $sample: { size: 1 } }]).toArray();
-        
+        const map = await client.db.collection("beatSaverLocal").aggregate([{ $match: { automapper: false } }, { $sample: { size: 1 } }]).toArray();
+
+        let njsString = "||";
+
+        console.log(map[0].versions[0]);
+        for (let i = 0; i < map[0].versions[0].diffs.length; i++) {
+            njsString+= `\n${map[0].versions[0].diffs[i].difficulty} - ${map[0].versions[0].diffs[i].njs}` 
+        }
+        njsString+= "||"
+
         const embed = new Discord.MessageEmbed()
             .addField(`\u200b`, `||${map[0].metadata.songName} ${map[0].metadata.songSubName} by ${map[0].metadata.levelAuthorName}||`)
             .addField(`\u200b`, `\`!bsr ${map[0].key}\``)
+            .addField(`\u200b`, `NJS: ${njsString}`)
             .addField(`\u200b`, `[Download](${map[0].versions[0].downloadURL}) | [BeatSaver](https://beatsaver.com/maps/${map[0].key.toLowerCase()}) | [Preview](https://skystudioapps.com/bs-viewer/?id=${map[0].key})`);
-        
+
         await message.channel.send(embed);
     }
 }
