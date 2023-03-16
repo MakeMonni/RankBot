eb4afc134f306a409aedd60d1c7509b96ec89e04
Make <makemake.nylund@gmail.com>
2023-03-16 20:58:17 +0200

    update stats command

--- commands/stats.js
+++ commands/stats.js
@@ -4,11 +4,14 @@ const Discord = require("discord.js");
 class Stats extends Command {
     async run(client, message, args) {
         const scoreStats = await client.db.collection("discordRankBotScores").stats();
+        //const withBeatSaviorStats = await client.db.collection("discordRankBotScores").find({ beatsavior: { $exists: true } }).count()
         const scoreBDsize = Math.round(scoreStats.size / 1048576 * 100) / 100;
         const distinctPlayers = await client.db.collection("discordRankBotScores").distinct("player");
 
         const songStats = await client.db.collection("beatSaverLocal").stats();
         const songDBsize = Math.round(songStats.size / 1048576 * 100) / 100;
+        const deletedCount = await client.db.collection("beatSaverLocal").find({ deleted: true }).count()
+        const automapperCount = await client.db.collection("beatSaverLocal").find({ automapper: true }).count()
 
         const rankedDiffCount = await client.db.collection("scoresaberRankedMaps").countDocuments();
         const rankedMapCount = await client.db.collection("scoresaberRankedMaps").distinct("hash");
@@ -16,9 +19,9 @@ class Stats extends Command {
         const userStats = await client.db.collection("discordRankBotUsers").stats();
 
         const embed = new Discord.MessageEmbed()
-            .setColor('#513dff')   
+            .setColor('#513dff')
             .addField(`Scores`, `${scoreStats.count} scores taking ${scoreBDsize}MB.\nFrom ${distinctPlayers.length} unique players.`)
-            .addField(`Songs`, `${songStats.count} maps taking ${songDBsize}MB.`)
+            .addField(`Songs`, `${songStats.count} maps taking ${songDBsize}MB.\nDeleted maps: ${deletedCount} \nAutomapped: ${automapperCount}`)
             .addField(`Ranked`, `${rankedMapCount.length} maps with ${rankedDiffCount} diffs.`)
             .addField(`Users`, `${userStats.count} users registered.`)
             .setTimestamp()
