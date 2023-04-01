dc771b1a82ffdc45d543ff1501e2a61451cfb317
Make <makemake.nylund@gmail.com>
2023-04-01 19:43:54 +0300

    remove april fools

--- core/handlers/commandHandler.js
+++ core/handlers/commandHandler.js
@@ -25,7 +25,6 @@ class CommandHandler {
                         return;
                     }
                     await cmd.run(this.client, message, args);
-                    if (this.client.config.easterEggs === "aprilFools") await message.channel.send(randomTatti())
                 }
                 catch (err) {
                     console.log(err);
@@ -38,23 +37,3 @@ class CommandHandler {
     }
 }
 module.exports = CommandHandler;
-
-function randomTatti() {
-    const links = [
-        "https://suomenluonto.fi/wp-content/uploads/2020/08/talitiaiset-1000x668.jpg",
-        "https://suomenluonto.fi/wp-content/uploads/2020/08/veritatti_lassekosonen-1000x718.jpg",
-        "https://suomenluonto.fi/wp-content/uploads/2020/08/kangastatti_jarkkokorhonen-1000x808.jpg",
-        "https://suomenluonto.fi/wp-content/uploads/2020/08/nokitatti_lassekosonen-1000x708.jpg",
-        "https://suomenluonto.fi/wp-content/uploads/2020/08/koivunpunikkitatti_lassekosonen-1000x918.jpg",
-        "https://suomenluonto.fi/wp-content/uploads/2020/08/samettitatti_timoviitanen.jpg",
-        "https://suomenluonto.fi/wp-content/uploads/2020/08/sappitatti_jarkkokorhonen.jpg",
-        "https://suomenluonto.fi/wp-content/uploads/2020/08/voitatti_jarkkokorhonen.jpg",
-        "https://suomenluonto.fi/wp-content/uploads/2020/08/ruskotatti_lassekosonen-1000x755.jpg",
-        "https://suomenluonto.fi/wp-content/uploads/2020/08/herkkutatti_jarkkokorhonen-1000x833.jpg",
-        "https://puutarha.net/artikkelit/img/20198/59606.jpg",
-        "https://puutarha.net/artikkelit/img/20198/59603.jpg",
-        "https://puutarha.net/artikkelit/img/20198/59607_p_ver-1.jpg"
-    ]
-    let r = Math.floor(Math.random() * links.length);
-    return links[r];
-}
