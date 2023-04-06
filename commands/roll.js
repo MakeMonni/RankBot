d1058d950da8c1af4b03554490f03f7b31ade41f
Make <makemake.nylund@gmail.com>
2023-04-07 00:58:58 +0300

    fix rolling decimals

--- commands/roll.js
+++ commands/roll.js
@@ -5,7 +5,7 @@ class Roll extends Command {
         if (!args[0]) {
             await message.channel.send(`${message.author.username} rolled ${Math.floor(Math.random() * 100+1)} out of 100.`)
         }
-        else if (isFinite(args[0]) && args[0] > 0) {
+        else if (isFinite(args[0]) && args[0] > 0 && args[0] % 1 === 0) {
             await message.channel.send(`${message.author.username} rolled ${Math.floor(Math.random() * args[0]+1)} out of ${args[0]}.`)
         }
         else {
