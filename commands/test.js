464c48a439af92f1395bd5e8049ca623b9eacc2d
GeneralDum <woimamaito@gmail.com>
2023-05-27 13:53:15 +0300

    un test

--- commands/test.js
+++ commands/test.js
@@ -1,5 +1,5 @@
 const Command = require("../core/command/command.js");
-// test
+
 class Test extends Command {
     async run(client, message, args) {
         await message.channel.send("Nice test yes.");
